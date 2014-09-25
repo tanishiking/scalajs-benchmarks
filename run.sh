56a8b5265b77abe14ae1c23cb0bdaa9d3fa97716
Sébastien Doeraene <sjrdoeraene@gmail.com>
2014-09-25 14:59:36 +0200

    Build everything with one sbt invocation.

--- run.sh
+++ run.sh
@@ -9,12 +9,8 @@ detect_engines $ENGINES
 for benchmark in $(find . -mindepth 2 -name "run.sh" | grep -vE "$EXCLUDE"); do
 	name="$(basename "$(dirname "$benchmark")")"
 	echo
-	info "$name [pack] sbt"
-	TIME="%E" time sbt "$name/packageJS" >/dev/null
-	info "$name [fastopt] sbt"
-	TIME="%E" time sbt "$name/fastOptJS" >/dev/null
-	info "$name [fullopt] sbt"
-	TIME="%E" time sbt "$name/fullOptJS" >/dev/null
+	info "$name [all] sbt"
+	TIME="%E" time sbt "$name/packageJS" "$name/fastOptJS" "$name/fullOptJS" >/dev/null
 
 	for mode in $MODES; do
 		for engine in $ENGINES; do
